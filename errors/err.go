4533657b3d292ea99bf8c1bf54981d66e83745b6
binary_soup <contact@binarysoup.dev>
2026-08-14 18:07:08 -0400

    Add Is func to check error type

--- errors/err.go
+++ errors/err.go
@@ -14,3 +14,9 @@ func New(msg string) error {
 func Format(format string, a ...any) error {
 	return fmt.Errorf(format, a...)
 }
+
+// Check if the error is the given the type.
+func Is[T error](err error) bool {
+	_, ok := err.(T)
+	return ok
+}
